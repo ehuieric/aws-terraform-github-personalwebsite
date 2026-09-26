df0dfad118eb3b83d1b0bb2f2e43d4cee72161c5
eric <ehui_eric@yahoo.fr>
2026-09-25 23:59:07 -0400

    using data to fetch zone id

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,7 +74,7 @@ resource "aws_route53_record" "ehuieric_cert_validation" {
     }
   }
 
-  zone_id = "data.aws_route53_record.domain_zone.zone_id"
+  zone_id = "data.aws_route53_zone.domain_zone.zone_id"
   name    = each.value.name
   records = [each.value.record]
   ttl     = 60
