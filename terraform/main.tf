a35d48c294e42c25b274bd12b5f0ae60aaa058e4
eric <ehui_eric@yahoo.fr>
2026-09-25 23:47:28 -0400

    using data to fetch zone id

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,3 +57,29 @@ resource "aws_acm_certificate" "ehuieric_cert" {
     create_before_destroy = true
   }
 }
+
+resource "aws_route53_record" "ehuieric_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.ehuieric_cert.domain_validation_options :
+    dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = "my zone id"
+  name    = each.value.name
+  records = [each.value.record]
+  ttl     = 60
+  type    = each.value.type
+}
+
+resource "aws_acm_certificate_validation" "ehuieric_cert_validation" {
+  certificate_arn = aws_acm_certificate.ehuieric_cert.arn
+
+  validation_record_fqdns = [
+    for record in aws_route53_record.ehuieric_cert_validation :
+    record.fqdn
+  ]
+}
